1d236db14682ed34edf4dba0209472a9df2a7a79
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2023-08-16 08:00:03 -0300

    questao 2

---
+++ questao_2.py
@@ -0,0 +1,9 @@
+import numpy as np
+from questao_1 import *
+
+array_3 = array_3.reshape(2,5)
+print(array_3)
+array_3 = array_3.astype(np.double)
+print(array_3)
+array_3 = array_3.T
+print(array_3)
