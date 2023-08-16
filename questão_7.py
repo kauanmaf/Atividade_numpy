08f1d006975225f11207b35c288cc854efb4b4a6
GUILHERME CARVALHO CUNHA <B51072@fgv.edu.br>
2023-08-16 08:38:15 -0300

    final questão 7

--- questão_7.py
+++ questão_7.py
@@ -1,7 +1,7 @@
 import numpy as np
 import numpy.random as npr
 
-ndarray8 = np.array([[4, 2, 3],[1, -4, 6], [-2, 3, -1]])
+ndarray8 = np.array([[1, 1, 1],[3, 5, 4], [3, 6, 5]])
 print("O array é:", ndarray8)
 
 print("\n", "#"*40, "\n")
@@ -16,9 +16,10 @@ print("O determinante é:", determinante)
 
 print("\n", "#"*40, "\n")
 
-inversa = np.linalg.inv(ndarray8)
+inversa = np.linalg.inv(ndarray8).astype(int)
 print("A inversa é:", inversa)
 
 print("\n", "#"*40, "\n")
 
 print(np.dot(ndarray8, inversa))
+print("O resultado da multiplicação é a identidade, logo, a corretude foi demonstrada")
