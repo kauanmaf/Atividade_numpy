323ed287ed2a26222964b34fa2c5e15f887ce486
GUILHERME CARVALHO CUNHA <B51072@fgv.edu.br>
2023-08-16 08:43:10 -0300

    transformei em funções

--- questão_6.py
+++ questão_6.py
@@ -1,9 +1,10 @@
 import numpy as np
 import numpy.random as npr
 
-vetor1 = np.array([1,2,3])
-vetor2 = np.array([4,5,6])
+def questão_6():
+    vetor1 = np.array([1,2,3])
+    vetor2 = np.array([4,5,6])
 
-produtovetorial = np.cross(vetor1, vetor2)
+    produtovetorial = np.cross(vetor1, vetor2)
+    print(produtovetorial)
 
-print(produtovetorial)
--- questão_7.py
+++ questão_7.py
@@ -1,25 +1,27 @@
 import numpy as np
 import numpy.random as npr
 
-ndarray8 = np.array([[1, 1, 1],[3, 5, 4], [3, 6, 5]])
-print("O array é:", ndarray8)
+def questão_7():
+    ndarray8 = np.array([[1, 1, 1],[3, 5, 4], [3, 6, 5]])
+    print("O array é:", ndarray8)
 
-print("\n", "#"*40, "\n")
+    print("\n", "#"*40, "\n")
 
-identidade = np.identity(3)
-print("A identidade é:", identidade)
+    identidade = np.identity(3)
+    print("A identidade é:", identidade)
 
-print("\n", "#"*40, "\n")
+    print("\n", "#"*40, "\n")
 
-determinante = np.linalg.det(ndarray8)
-print("O determinante é:", determinante)
+    determinante = np.linalg.det(ndarray8)
+    print("O determinante é:", determinante)
 
-print("\n", "#"*40, "\n")
+    print("\n", "#"*40, "\n")
 
-inversa = np.linalg.inv(ndarray8).astype(int)
-print("A inversa é:", inversa)
+    inversa = np.linalg.inv(ndarray8).astype(int)
+    print("A inversa é:", inversa)
 
-print("\n", "#"*40, "\n")
+    print("\n", "#"*40, "\n")
 
-print(np.dot(ndarray8, inversa))
-print("O resultado da multiplicação é a identidade, logo, a corretude foi demonstrada")
+    print(np.dot(ndarray8, inversa))
+    print("O resultado da multiplicação é a identidade, logo, a corretude foi demonstrada")
+    
