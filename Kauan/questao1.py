26c6c257d0bf0d3c0fcc1e5881228edf90158c6b
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2023-08-16 09:07:57 -0300

    tudo

--- Kauan/questao1.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import numpy.random as npr
-
-Array_1d_1 = np.arange(12)
-Array_1d_2 = npr.randint(0,100,12)
-Array_1d_3 = Array_1d_1 + Array_1d_2
---
+++ main.py
@@ -0,0 +1,12 @@
+from questao_1 import *
+from questao_2 import *
+from questao_3 import *
+from questao_4 import *
+from questao_5 import *
+from questão_6 import *
+from questão_7 import *
+
+prints_questao_4()
+prints_questao_5()
+questão_6()
+questão_7()
