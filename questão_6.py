5d5eeb01d13380eded82e1376a50aed682749c04
GUILHERME CARVALHO CUNHA <B51072@fgv.edu.br>
2023-08-16 08:48:23 -0300

    final questão 6

--- questão_6.py
+++ questão_6.py
@@ -2,9 +2,10 @@ import numpy as np
 import numpy.random as npr
 
 def questão_6():
-    vetor1 = np.array([1,2,3])
-    vetor2 = np.array([4,5,6])
+    vetor1 = np.array([[1,2,3],[8,7,5]])
+    vetor2 = np.array([[4,5,6],[2,5,3]])
 
     produtovetorial = np.cross(vetor1, vetor2)
-    print(produtovetorial)
+
+    print("O resultado do produto dos dois vetores é:\n",produtovetorial)
 
