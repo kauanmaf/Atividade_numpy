982cc5c495a1227f0513503a8a28347764b16971
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2023-08-16 08:08:52 -0300

    questao 3

---
+++ questao_3.py
@@ -0,0 +1,9 @@
+import numpy as np
+from questao_2 import *
+
+# array com numeros inteiros
+array_4 = np.random.randint(100, size = 10)
+# fazendo shape igual ao array 3
+array_4 = array_4.reshape(5,2)
+# multiplicando arrays
+array_4 = array_3 * array_4
